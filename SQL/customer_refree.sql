7e3b927b6509820fcd41331a872d985144bf5abd
mk3709 <91836965+mk3709@users.noreply.github.com>
2023-02-27 21:05:49 +0530

    Create customer_refree.sql

---
+++ SQL/customer_refree.sql
@@ -0,0 +1,24 @@
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+
+
+SELECT name from customer where  referee_id != 2 or referee_id is NULL ; 
